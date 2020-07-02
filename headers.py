3e0e538c9b838e97e63e43e3c0a13c8e01cdaeec
DhanrajHira <hiradhanraj0100@gmail.com>
2020-07-02 12:49:48 +0530

    trimmed down non required headers and cookies

--- headers.py
+++ headers.py
@@ -1,21 +1,12 @@
 class Cookies(object):
-    cookies = {#'__cfduid' : 'da4531e447728e6cd3933478e47cf95d21592970354',
-    #'LANGUAGE' : 'en-US	',
-    #'_ga' : 'GA1.2.2088838248.1592970354',
-    #'ANIWATCH_CHAT_SETTINGS' : r'%7B%22userlist_collapsed%22%3Atrue%2C%22scroll_msg%22%3Atrue%2C%22show_time%22%3Atrue%2C%22parse_smileys%22%3Atrue%2C%22show_system_msg%22%3Atrue%2C%22new_msg_beep_sound%22%3Afalse%2C%22auto_connect%22%3Atrue%2C%22retry_reconnection%22%3Atrue%7D',
+    cookies = {
     'SESSION' : r'%7B%22userid%22%3A3407%2C%22username%22%3A%22DBHIRA%22%2C%22usergroup%22%3A4%2C%22player_lang%22%3A1%2C%22player_quality%22%3A1%2C%22player_time_left_side%22%3A2%2C%22player_time_right_side%22%3A3%2C%22screen_orientation%22%3A1%2C%22nsfw%22%3A0%2C%22chrLogging%22%3A1%2C%22mask_episode_info%22%3A0%2C%22blur_thumbnails%22%3A0%2C%22autoplay%22%3A1%2C%22preview_thumbnails%22%3A1%2C%22update_watchlist%22%3A1%2C%22update_watchlist_notification%22%3A1%2C%22playheads%22%3A1%2C%22hide_chat%22%3A1%2C%22seek_time%22%3A5%2C%22update_watchlist_percentage%22%3A80%2C%22cover%22%3Anull%2C%22title%22%3A%22Member%22%2C%22premium%22%3A1%2C%22lang%22%3A%22en-US%22%2C%22auth%22%3A%22cWZycGtpbG5sUk82dFBFSGRxR2cxZz09%22%2C%22remember_login%22%3Atrue%7D',
-    #'_gid' : 'GA1.2.673758088.1593595489',
     'XSRF-TOKEN	' : '1c02c094d0e6191c05c5b7ae1a8bc2d7',
-    #'_gat' : '1	',
     }
 
 class Head(object):
     headers = {
-        #'accept': 'application/json, text/plain, */*',
-        #'accept-encoding': 'gzip, deflate, br',
-        #'accept-language': 'en-US,en;q=0.9',
-        #'content-type': 'application/json;charset=UTF-8',
-        #'cookie': r'SESSION=%7B%22userid%22%3A3407%2C%22username%22%3A%22DBHIRA%22%2C%22usergroup%22%3A4%2C%22player_lang%22%3A1%2C%22player_quality%22%3A1%2C%22player_time_left_side%22%3A2%2C%22player_time_right_side%22%3A3%2C%22screen_orientation%22%3A1%2C%22nsfw%22%3A0%2C%22chrLogging%22%3A1%2C%22mask_episode_info%22%3A0%2C%22blur_thumbnails%22%3A0%2C%22autoplay%22%3A1%2C%22preview_thumbnails%22%3A1%2C%22update_watchlist%22%3A1%2C%22update_watchlist_notification%22%3A1%2C%22playheads%22%3A1%2C%22hide_chat%22%3A1%2C%22seek_time%22%3A5%2C%22update_watchlist_percentage%22%3A80%2C%22cover%22%3Anull%2C%22title%22%3A%22Member%22%2C%22premium%22%3A1%2C%22lang%22%3A%22en-US%22%2C%22auth%22%3A%22cWZycGtpbG5sUk82dFBFSGRxR2cxZz09%22%2C%22remember_login%22%3Atrue%7D; XSRF-TOKEN=1c02c094d0e6191c05c5b7ae1a8bc2d7;',
+        'cookie': r'SESSION=%7B%22userid%22%3A3407%2C%22username%22%3A%22DBHIRA%22%2C%22usergroup%22%3A4%2C%22player_lang%22%3A1%2C%22player_quality%22%3A1%2C%22player_time_left_side%22%3A2%2C%22player_time_right_side%22%3A3%2C%22screen_orientation%22%3A1%2C%22nsfw%22%3A0%2C%22chrLogging%22%3A1%2C%22mask_episode_info%22%3A0%2C%22blur_thumbnails%22%3A0%2C%22autoplay%22%3A1%2C%22preview_thumbnails%22%3A1%2C%22update_watchlist%22%3A1%2C%22update_watchlist_notification%22%3A1%2C%22playheads%22%3A1%2C%22hide_chat%22%3A1%2C%22seek_time%22%3A5%2C%22update_watchlist_percentage%22%3A80%2C%22cover%22%3Anull%2C%22title%22%3A%22Member%22%2C%22premium%22%3A1%2C%22lang%22%3A%22en-US%22%2C%22auth%22%3A%22cWZycGtpbG5sUk82dFBFSGRxR2cxZz09%22%2C%22remember_login%22%3Atrue%7D; XSRF-TOKEN=1c02c094d0e6191c05c5b7ae1a8bc2d7;',
         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36',
         'x-auth': 'cWZycGtpbG5sUk82dFBFSGRxR2cxZz09',
         'x-xsrf-token': '1c02c094d0e6191c05c5b7ae1a8bc2d7'
